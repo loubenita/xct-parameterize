b3009b907f326f23ae7cafa5f40874ee9178a936
Lenard Pop <denisz.pop@gmail.com>
2025-05-05 10:08:36 +0100

    Chore: Updating to the latest SwiftSyntax package

--- Package.swift
+++ Package.swift
@@ -8,20 +8,15 @@ let package = Package(
     name: "XCTParameterize",
     platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "XCTParameterize",
             targets: ["XCTParameterize"]
         )
     ],
     dependencies: [
-        // Depend on the Swift 5.9 release of SwiftSyntax
-        .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0"),
+        .package(url: "https://github.com/apple/swift-syntax.git", from: "601.0.1"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        // Macro implementation that performs the source transformation of a macro.
         .macro(
             name: "XCTMacros",
             dependencies: [
@@ -29,19 +24,13 @@ let package = Package(
                 .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
             ]
         ),
-
-        // Library that exposes a macro as part of its API, which is used in client programs.
         .target(name: "XCTParameterize", dependencies: ["XCTMacros"]),
-
-        // A client of the library, which is able to use the macro in its own code.
         .executableTarget(
             name: "XCTClient",
             dependencies: [
                 "XCTParameterize"
             ]
         ),
-
-        // A test target used to develop the macro implementation.
         .testTarget(
             name: "XCTTests",
             dependencies: [
